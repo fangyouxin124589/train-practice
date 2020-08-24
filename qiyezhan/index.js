73a9f6f02ec89840343019539faf30b31953719b
fangyouxin124589 <1277303192@qq.com>
2020-08-24 15:39:33 +0800

    企业站完成

---
+++ qiyezhan/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".header_right_app").click(function () {
+      $(".nav_app").slideToggle(500);
+    });
+  
+    $(function () {
+      $("img.lazy").lazyload({ effect: "fadeIn" });
+    });
+  });
+  
--- qiyezhan/styles.js
+++ qiyezhan/styles.js
@@ -2,8 +2,4 @@ $(document).ready(function () {
   $(".header_right_app").click(function () {
     $(".nav_app").slideToggle(500);
   });
-
-  $(function () {
-    $("img.lazy").lazyload({ effect: "fadeIn" });
-  });
 });
